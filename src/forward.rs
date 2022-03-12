9590df716a0814f1b36565587f9712eba17ec96c
ryan kurte <ryankurte@gmail.com>
2022-03-12 19:32:21 +1300

    Add defmt support

--- src/forward.rs
+++ src/forward.rs
@@ -49,6 +49,7 @@ impl<T> Forward<T> {
 ///
 /// This fulfils error trait bounds but `.kind()` always returns `Other`
 #[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct ForwardError<E>(pub E);
 
 // note that implementations over Forward cannot be generic over word type
